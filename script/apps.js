99a31746cf8215a94f902dda1a72e6bf93bae0f9
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-02-18 08:33:46 -0800

    Function

--- script/apps.js
+++ script/apps.js
@@ -4,6 +4,8 @@ let name = 'Layton';
 let sum = 12 + 10;
 let divide = 10 / 5;
 
+function letFunction(){
 console.log('name:', name);
 console.log('sum of 12 + 10:', sum);
 console.log('10 / 5 is:', divide);
+}
--- script/function.js
+++ script/function.js
@@ -1,7 +1,12 @@
-function letCommand (
-let ,name = 'Layton' ,
-let ,sum = 12 + 10,
-let ,divide = 10 / 5,
-)
+let name = 'Layton';
+let sum = 12 + 10;
+let divide = 10 / 5;
 
-console.log (letCommand)
+
+function partOne(){
+    console.log('name: ',name)
+    console.log('The sum of 12+10: ', sum)
+    console.log('10 / 2 is: ', divide)
+}
+
+partOne();
